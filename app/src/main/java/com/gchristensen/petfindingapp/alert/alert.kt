33fcc48ca12994e47450decd39821c4d90671398
Gage Christensen <grchr4000@gmail.com>
2020-06-06 09:02:04 -0700

    alert now just uses functions in a package

---
+++ app/src/main/java/com/gchristensen/petfindingapp/alert/alert.kt
@@ -0,0 +1,21 @@
+package com.gchristensen.petfindingapp.alert
+
+import com.gchristensen.petfindingapp.Location
+import com.gchristensen.petfindingapp.Picture
+import com.gchristensen.petfindingapp.Time
+
+/**
+ * Should generate a message and send it to the server.
+ * */
+fun generateReport(content: String, time: Time, location: Location) {
+
+
+    TODO()
+}
+
+/**
+ * Generates a message and includes a picture
+ * */
+fun generateReportWithPicture(content: String, time: Time, location: Location, picture: Picture) {
+    TODO()
+}
