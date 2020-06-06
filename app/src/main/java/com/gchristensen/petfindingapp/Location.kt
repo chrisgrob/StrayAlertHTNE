61ccbce16b12525c73fe93cc0b646e96f0a783cb
Gage Christensen <grchr4000@gmail.com>
2020-06-05 17:21:56 -0700

    Android location now considered

--- app/src/main/java/com/gchristensen/petfindingapp/Location.kt
+++ app/src/main/java/com/gchristensen/petfindingapp/Location.kt
@@ -1,6 +1,15 @@
 package com.gchristensen.petfindingapp
 
+import android.location.Location as AndroidLocation
+
 /**
  * Wrapper around GPS Location to provide convenient services
  * */
-class Location {}
+class Location(private val androidLocation: AndroidLocation) {
+    /**
+     * Gives the distance from another location.
+     * */
+    fun distanceFrom(other: Location): Int {
+        return androidLocation.distanceTo(other.androidLocation).toInt()
+    }
+}
