74c97252a0a01e9d1bc59304fddd19ef80fc774c
Gage Christensen <grchr4000@gmail.com>
2020-06-05 17:55:54 -0700

    API

---
+++ app/src/main/java/com/gchristensen/petfindingapp/Alert.kt
@@ -0,0 +1,20 @@
+package com.gchristensen.petfindingapp
+
+/**
+ * Black box class for generating alerts
+ * */
+class Alert {
+    /**
+     * Should generate a message and send it to the server.
+     * */
+    fun standard(content: String, time: Time, location: Location) {
+        TODO()
+    }
+
+    /**
+     * Generates a message and includes a picture
+     * */
+    fun withPicture(content: String, time: Time, location: Location, picture: Picture) {
+        TODO()
+    }
+}
